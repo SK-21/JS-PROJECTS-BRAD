57f3926a817be7758ddc044bca302135e9d264be
Sapna Kashyap <sapnakashyap2109@gmail.com>
2023-05-31 23:02:15 +0530

    first commit in form validation

---
+++ form-validation/script.js
@@ -0,0 +1,5 @@
+const userame=document.getElementById("username")
+const email=document.getElementById("email")
+const password=document.getElementById("pass")
+const cPassword=document.getElementById("c-pass")
+
