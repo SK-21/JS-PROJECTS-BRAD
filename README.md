da989879491faa65ad66c555ff431d1d0ddd5b0c
Sapna Kashyap <sapnakashyap2109@gmail.com>
2023-06-01 22:06:00 +0530

    new commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JS-PROJECTS-BRAD
+
+## Here I am adding all the projects of Brad creating on vanila js
+
+- Project 1- form validatiion
--- form-validation/script.js
+++ form-validation/script.js
@@ -3,3 +3,7 @@ const email=document.getElementById("email")
 const password=document.getElementById("pass")
 const cPassword=document.getElementById("c-pass")
 
+function submitButton(){
+
+}
+
